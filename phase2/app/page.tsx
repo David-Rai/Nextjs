bada4a6c2a36acedf7cde5d935c3a4acc3349389
david-rai <davidkhaling123@gmail.com>
2025-07-13 21:38:22 +0545

    Caching done

--- phase2/app/page.tsx
+++ phase2/app/page.tsx
@@ -1,14 +1,28 @@
-"use client"
 
-import { useState } from "react";
+interface user {
+  id: number,
+  name: string
+  email: string
+}
 
-export default function Page() {
-const [count,setCount]=useState<number>(0)
+export default async function Page() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const users: user[] = await res.json()
 
   return (
-    <main className="h-screen w-full flex items-center justify-center flex-col gap-5">
-      <h1>count - {count}</h1>
-      <button onClick={()=> setCount(count + 1)}>increase</button>
+    <main>
+      <h1>{new Date().toLocaleTimeString()}</h1>
+      {
+
+        users.map(user => {
+          return (
+            <div key={user.id}>
+              <h1>{user.name}</h1>
+              <p>{user.email}</p>
+            </div>
+          )
+        })
+      }
     </main>
   );
 }
