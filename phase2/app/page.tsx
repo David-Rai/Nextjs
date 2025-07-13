13c5e0a3316fc92536f99ea000b47ba4875d84fa
david-rai <davidkhaling123@gmail.com>
2025-07-13 21:41:07 +0545

    Day 2 of learnign next js

--- phase2/app/page.tsx
+++ phase2/app/page.tsx
@@ -6,7 +6,7 @@ interface user {
 }
 
 export default async function Page() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const res = await fetch('https://jsonplaceholder.typicode.com/users',{cache:'no-store'})
   const users: user[] = await res.json()
 
   return (
