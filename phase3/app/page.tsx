cff8c82a970332ceaad55210766315e7622cb019
david-rai <davidkhaling123@gmail.com>
2025-07-15 20:22:44 +0545

    day 4 add the scrappy frontend

--- phase3/app/page.tsx
+++ phase3/app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 import { useEffect, useRef, useState } from "react";
+import { MdDeleteOutline } from "react-icons/md";
+import { IoMdAdd } from "react-icons/io";
 
 export default function Page() {
   const [todos, setTodos] = useState([])
@@ -57,23 +59,33 @@ export default function Page() {
     getAll()
   }
 
+  //Handling key down
+  const handleKey = (e: KeyboardEvent) => {
+    // console.log(e)
+    if (e.code === "Enter") {
+      handleAdd()
+    }
+  }
+
   // MAIN JSX
   return (
-    <main className="flex h-screen w-full items-center justify-center flex-col">
-      <h1>Todo lists</h1>
+    <main className="flex h-screen w-full items-center justify-center flex-col bg-gray-200">
+      <h1 className="text-2xl ">Todo list</h1>
 
 
       {/* All todos */}
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-4">
         {
           todos.length > 0 && todos.map((todo, index) => {
             return (
-              <div key={index}>
+              <div key={index} className=" flex bg-white px-3 rounded-md h-[40px] items-center justify-between">
                 <li>
                   {
                     todo
                   }</li>
-                <button onClick={() => handleDelete(index)}>Delete</button>
+                <button onClick={() => handleDelete(index)} className="ml-3 text-red-500">
+                  <MdDeleteOutline />
+                </button>
               </div>
             )
           })
@@ -81,9 +93,13 @@ export default function Page() {
       </div>
 
       {/* Add new todo */}
-      <div>
-        <input type="text" placeholder="New todo" ref={inputRef} />
-        <button onClick={handleAdd}>Add</button>
+      <div className="flex gap-5 bg-gray-100 h-[50px] rounded-md items-center justify-between px-3 m-4">
+        <input type="text" placeholder="New todo"
+          onKeyDown={()=>handleKey}
+          ref={inputRef} className="focus:border-0 focus:outline-0" />
+        <button onClick={handleAdd}>
+          <IoMdAdd size={20} />
+        </button>
       </div>
     </main>
   );
