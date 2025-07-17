54093b860e6664fb6675e375813c1fe8d9fce4eb
david-rai <davidkhaling123@gmail.com>
2025-07-17 13:26:37 +0545

    day 6 still in nextclone landing page

--- nextclone/components/ui/own/Hero.tsx
+++ nextclone/components/ui/own/Hero.tsx
@@ -1,6 +1,6 @@
 import vercel from '../../.././public/vercel.svg'
 import { Button } from "@/components/ui/button"
-import Image from 'next/Image'
+import Image from "next/image";
 
 export default function Page() {
     return (
